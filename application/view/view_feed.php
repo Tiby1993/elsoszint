ef5da16cb12a6618d2d689e7e52b0ed150f5b019
Tiby1993 <Turai.Tibor.Adam@stud.u-szeged.hu>
2016-11-15 23:34:19 +0100

    Add files via upload

---
+++ application/view/view_feed.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+   <title>Login</title>
+    <link rel="stylesheet" href="../application/include/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+ </head>
+ <body>
+ 	<div class="activity-feed">
+  <div class="feed-item">
+    <div class="date">Sep 25</div>
+    <div class="text">Responded to need "Volunteer opportunity”</div>
+  </div>
+</div>
+ </body>
+</html>
