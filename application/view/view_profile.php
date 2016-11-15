25c0de72c4b2d9925498be4b4e236701f876bd05
Tiby1993 <Turai.Tibor.Adam@stud.u-szeged.hu>
2016-11-16 00:25:57 +0100

    Add files via upload

--- application/view/view_profile.php
+++ application/view/view_profile.php
@@ -1,10 +1,41 @@
+<!DOCTYPE html>
 <html>
-    <head>
-   <title>Login</title>
-    <link rel="stylesheet" href="../application/include/style.css">
-	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
- </head>
- <body>
+<head>
+<title>Olimpiai honlap</title>
+<link rel="stylesheet" href="../application/include/style.css">
+<center><canvas id=c1 width=400 height=200></canvas>
+<script>
+    ctx = c1.getContext('2d')
+
+    ctx.fillRect(0, 0, 400, 400)
+    ctx.fillStyle = '#FFF'
+    ctx.fillRect(10, 10, 400-20, 200-20)
+
+    var X = 75, W = 50, G = 20
+    ctx.lineWidth = 10
+    var colors = ['blue', 'black', 'red', 'yellow', 'green']
+    var args = [
+        [X,X,W],
+        [X+W+W+G,X,W],
+        [X+W+W+G+W+W+G,X,W],
+        [X+W+G/2,X+W,W],
+        [X+W+G/2+W+W+G,X+W,W]]
+
+    while (colors.length > 0) {
+        ctx.strokeStyle = colors.shift()
+        ctx.beginPath()
+        ctx.arc.apply(ctx, args.shift().concat([0,Math.PI*2,true]))
+        ctx.stroke()
+    }
+</script>
+<ul>
+  <li><a href="/elsoszint/login/index/">Kezdőlap</a></li>
+  <li><a href="/elsoszint/feed/index">Hírfolyam</a></li>
+  <li><a href="#contact">Rólunk</a></li>
+  <li><a href="#about">Bejelentkezés</a></li>
+</ul></head>
+<body>
+<br />
  	<div class="profile">
 		<div class="header"><p><?php echo $data['user'];?></p></div>
 		<div class="pic"><img src="../application/include/avatar.png"></div>
@@ -30,5 +61,14 @@
 		</div>	
 	</div>
 	</form>
+	<br />
+	<footer>
+<ul>
+  <li><a href="#home">Kezdőlap</a></li>
+  <li><a href="#news">Hírfolyam</a></li>
+  <li><a href="#contact">Rólunk</a></li>
+  <li><a href="#about">Bejelentkezés</a></li>
+</ul>
+</footer>
  </body>
 </html>
--- application/view/view_registration.php
+++ application/view/view_registration.php
@@ -1,12 +1,41 @@
-﻿<html>
- <head>
-   <title>Regisztráció</title>
-    <link rel="stylesheet" href="../application/include/style.css">
-	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
- </head>
- <body>
-	
-	<h1>Regisztráció</h1>
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>Olimpiai honlap</title>
+<link rel="stylesheet" href="../application/include/style.css">
+<center><canvas id=c1 width=400 height=200></canvas>
+<script>
+    ctx = c1.getContext('2d')
+
+    ctx.fillRect(0, 0, 400, 400)
+    ctx.fillStyle = '#FFF'
+    ctx.fillRect(10, 10, 400-20, 200-20)
+
+    var X = 75, W = 50, G = 20
+    ctx.lineWidth = 10
+    var colors = ['blue', 'black', 'red', 'yellow', 'green']
+    var args = [
+        [X,X,W],
+        [X+W+W+G,X,W],
+        [X+W+W+G+W+W+G,X,W],
+        [X+W+G/2,X+W,W],
+        [X+W+G/2+W+W+G,X+W,W]]
+
+    while (colors.length > 0) {
+        ctx.strokeStyle = colors.shift()
+        ctx.beginPath()
+        ctx.arc.apply(ctx, args.shift().concat([0,Math.PI*2,true]))
+        ctx.stroke()
+    }
+</script>
+<ul>
+  <li><a href="/elsoszint/login/index/">Kezdőlap</a></li>
+  <li><a href="/elsoszint/feed/index">Hírfolyam</a></li>
+  <li><a href="#contact">Rólunk</a></li>
+  <li><a href="#about">Bejelentkezés</a></li>
+</ul></head>
+<body>
+<br />
 	<div class="container">
 		<form id="registration" action="" method="post">
 		<div class="input-group">
@@ -47,5 +76,16 @@
 			<button type="submit" class="button">Submit</button>
 	</div>
 	</form>
+	<br />
+	<footer>
+<ul>
+  <li><a href="#home">Kezdőlap</a></li>
+  <li><a href="#news">Hírfolyam</a></li>
+  <li><a href="#contact">Rólunk</a></li>
+  <li><a href="#about">Bejelentkezés</a></li>
+</ul>
+</footer>
+ </body>
+</html>
  </body>
 </html>
